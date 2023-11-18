e990d46ac9bf2a416355643ab9d6f895f974715f
Ariel Ferensztajn <77733595+El19881@users.noreply.github.com>
2023-11-18 19:55:21 +0100

    Update main.py
    Updated:
    - fixed issue with exit game process
    - counting score
    - leaf movement and buffer'
    - assets addition

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ pygame.init()
 
 
 #display surface
-window_width = 1000
-window_height = 600
+window_width = 1680
+window_height = 1050
 
-display_surface = pygame.display.set_mode(window_width, window_height)
-pygame.display_caption("Feed the dino!")
+display_surface = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Feed the dino!")
+
+background = pygame.image.load("display.jpg")
 
 #fps and clock
 fps = 60
@@ -16,14 +18,14 @@ clock = pygame.time.Clock()
 
 #game values
 player_starting_lives = 3
-velocity = 5
-egg_startring_velocity = 3
-egg_acceleration = 0.25
-buffer_distance = 100
+velocity = 10
+leaf_starting_velocity = 6
+leaf_acceleration = 0.25
+buffer_distance = 400
 
 score = 0
 player_lives = player_starting_lives
-egg_velocity = velocity
+leaf_velocity = 6
 
 #set colors
 green = (0,255,0)
@@ -33,14 +35,14 @@ black = (0,0,0)
 
 
 #set fonts
-font = pygame.font.Font(AAAAAAAA, 32)
+font = pygame.font.Font('font.ttf', 32)
 
 #set text
 score_info = font.render("Score: " + str(score), True, green, darkgreen )
 score_rect = score_info.get_rect()
 score_rect.topleft = (10,10)
 
-title = font.rener("Feed the dino!", True, green, white)
+title = font.render("Feed the dino!", True, green, white)
 title_rect = title.get_rect()
 title_rect.centerx = (window_width//2)
 title_rect.y = 10
@@ -58,21 +60,21 @@ play_again_rect = play_again.get_rect()
 play_again_rect.center = (window_width//2, window_height//2 + 30)
 
 #set sounds and music
-egg_sound = pygame.mixer.Sound(AAAAAAA)
-miss_sound = pygame.mixer.Sound(AAAAAAAA)
+leaf_catch_sound = pygame.mixer.Sound("catch.wav")
+miss_sound = pygame.mixer.Sound("miss.wav")
 miss_sound.set_volume(.1)
-pygame.mixer.music.load(AAAAAAAA)
+pygame.mixer.music.load("song.wav")
 
 #set images
-player_image = pygame.image.load(AAAAAAA)
+player_image = pygame.image.load("dino.png")
 player_image_rect = player_image.get_rect()
 player_image_rect.left = 32
-player_image_rect.center = window_height//2
+player_image_rect.centery = window_height//2
 
-coin_image = pygame.image.load(AAAAAA)
-egg_rect = coin_image.get_rect()
-egg_rect.x = window_width + buffer_distance
-egg_rect.y = random.randint(64, window_height - 32)
+leaf_image = pygame.image.load("leaf.png")
+leaf_rect = leaf_image.get_rect()
+leaf_rect.x = window_width + buffer_distance
+leaf_rect.y = random.randint(64, window_height - 256)
 
 
 
@@ -82,7 +84,7 @@ pygame.mixer.music.play(-1,0.0)
 running = True
 while running:
     for event in pygame.event.get():
-        if event == pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
 
     #check if key is pressed
@@ -98,50 +100,55 @@ while running:
 
 
     #move coin
-    if egg_rect.x < 0:
+    if leaf_rect.x < 0:
         #player missed coin
         player_lives -= 1
         miss_sound.play()
-        egg_rect.x = window_width + buffer_distance
-        egg_rect.y = random.randint(64, window_height - 32)
+        leaf_rect.x = window_width + buffer_distance
+        leaf_rect.y = random.randint(64, window_height - 256)
     else:
-        egg_rect.x -= egg_velocity
+        leaf_rect.x -= leaf_velocity
 
     #check for collisions
-    if player_image_rect.colliderect(egg_rect):
-        score =+ 1
-        egg_sound.play()
-        egg_velocity =+ egg_acceleration
-        egg_rect.x = window_width + buffer_distance
-        egg_rect.y = random.randint(64, window_height - 32)
+    if player_image_rect.colliderect(leaf_rect):
+        score += 1
+        leaf_catch_sound.play()
+        leaf_velocity += leaf_acceleration
+        leaf_rect.x = window_width + buffer_distance
+        leaf_rect.y = random.randint(64, window_height - 256)
 
     #update text for score/lives
-    score_info = font.render("Score: " + str(score), True, green, darkgreen )
-    lives = font.render("Lives: " + str(player_lives), True, green, darkgreen)
+    score_info = font.render("Score " + str(score), True, green, darkgreen )
+    lives = font.render("Lives " + str(player_lives), True, green, darkgreen)
 
 
     #check for game over
     if player_lives == 0:
         display_surface.blit(game_over, game_over_rect)
         display_surface.blit(play_again, play_again_rect)
+        pygame.display.update()
 
         #pause the game until player preses a key and rest
-        pygame.mixed.music.stop()
-        is_paused = True:
+        pygame.mixer.music.stop()
+        is_paused = True
         while is_paused:
             for event in pygame.event.get():
                 #player wants to play again:
                 if event.type == pygame.KEYDOWN:
-                    #restart all text/metrics/score/lives/play music again
+                    score = 0
+                    player_lives = player_starting_lives
+                    player_image_rect.y = window_height//2
+                    leaf_velocity = leaf_starting_velocity
                     is_paused = False
+                    pygame.mixer_music.play(-1,0.0)
                 #player wants to quit
-                if event.type == pygame.QUIT:
+                elif event.type == pygame.QUIT:
                     is_paused = False
-                    is_running = False
+                    running = False
 
 
     #fill the display
-    display_surface.fill(black)
+    display_surface.blit(background,(0,0))
 
     #bilt the hud
     display_surface.blit(score_info, score_rect)
@@ -151,7 +158,7 @@ while running:
 
      #blit assets to screen
     display_surface.blit(player_image, player_image_rect)
-    display_surface.blit(coin_image, egg_rect) 
+    display_surface.blit(leaf_image, leaf_rect) 
 
     #update display and tick clok
     pygame.display.update()
